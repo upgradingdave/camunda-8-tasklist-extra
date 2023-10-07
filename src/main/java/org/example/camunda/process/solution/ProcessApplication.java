86152c99bbd7ae20dbeb73b105c9650caff00166
Dave Paroulek <david.paroulek@camunda.com>
2023-10-07 12:52:07 -0400

    Added sample application.yaml files

--- src/main/java/org/example/camunda/process/solution/ProcessApplication.java
+++ src/main/java/org/example/camunda/process/solution/ProcessApplication.java
@@ -1,13 +1,11 @@
 package org.example.camunda.process.solution;
 
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
-import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZeebeClient
-@Deployment(resources = "classpath*:/models/*.*")
+//@EnableZeebeClient
+//@Deployment(resources = "classpath*:/models/*.*")
 public class ProcessApplication {
 
   public static void main(String[] args) {
