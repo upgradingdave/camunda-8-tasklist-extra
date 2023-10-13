40e77f47be763d11aae2507eb7009355c5f899dc
Dave Paroulek <david.paroulek@camunda.com>
2023-10-12 21:02:44 -0400

    Add extra api call to load form schemas from local bpmn files

--- src/main/java/io/camunda/tasklist/facade/FormController.java
+++ src/main/java/io/camunda/tasklist/facade/FormController.java
@@ -4,6 +4,7 @@ import io.camunda.tasklist.rest.TaskListRestApi;
 import io.camunda.tasklist.rest.dto.FormResponse;
 import io.camunda.tasklist.rest.exception.TaskListException;
 import io.camunda.tasklist.rest.exception.TaskListRestException;
+import io.camunda.tasklist.service.BpmnUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,10 +24,18 @@ public class FormController {
   }
 
   @GetMapping("/{formId}")
-  public FormResponse startProcessInstance(
+  public FormResponse getFormById(
       @PathVariable String formId,
       @RequestParam("processDefinitionKey") String processDefinitionKey)
       throws TaskListException, TaskListRestException {
     return taskListRestApi.getForm(processDefinitionKey, formId);
+  }
+
+  @GetMapping("/{bpmnFileName}/{formId}")
+  public FormResponse getLocalForm(@PathVariable String bpmnFileName, @PathVariable String formId) {
+    String schema = BpmnUtils.getFormSchemaFromFile(bpmnFileName + ".bpmn", formId);
+    FormResponse formResponse = new FormResponse();
+    formResponse.setSchema(schema);
+    return formResponse;
   }
 }
---
+++ src/main/java/io/camunda/tasklist/service/BpmnUtils.java
@@ -0,0 +1,116 @@
+package io.camunda.tasklist.service;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class BpmnUtils {
+
+  private BpmnUtils() {}
+
+  private static final Logger LOG = LoggerFactory.getLogger(BpmnUtils.class);
+
+  private static Node getNodeFromBpmnFile(String bpmnFileName, String nodeId) {
+    InputStream bpmnIs =
+        BpmnUtils.class.getClassLoader().getResourceAsStream("bpmn/" + bpmnFileName);
+    return getNodeFromBpmn(bpmnIs, nodeId);
+  }
+
+  private static Node getNodeFromBpmn(String xml, String nodeId) {
+    return getNodeFromBpmn(
+        new ByteArrayInputStream(xml.getBytes(Charset.forName("UTF-8"))), nodeId);
+  }
+
+  private static Node getNodeFromBpmn(InputStream bpmnInputStream, String nodeId) {
+    try {
+      DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder builder = builderFactory.newDocumentBuilder();
+
+      Document xmlDocument = builder.parse(bpmnInputStream);
+      XPath xPath = XPathFactory.newInstance().newXPath();
+      String expression = "//*[@id=\"" + nodeId + "\"]";
+      NodeList nodeList =
+          (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
+      if (nodeList != null && nodeList.getLength() == 1) {
+        return nodeList.item(0);
+      }
+    } catch (IOException
+        | XPathExpressionException
+        | SAXException
+        | ParserConfigurationException e) {
+      LOG.error("Error reading the requested node id " + nodeId, e);
+    }
+    return null;
+  }
+
+  public static String getProcessName(String xml, String bpmnProcessId) {
+    LOG.info("get ProcessName for " + bpmnProcessId);
+    String processName = bpmnProcessId;
+    Node processNode = getNodeFromBpmn(xml, bpmnProcessId);
+    if (processNode != null) {
+      processName = processNode.getAttributes().getNamedItem("name").getNodeValue();
+    }
+    return processName;
+  }
+
+  public static String getTaskNameFromBpmn(String xml, String activityId) {
+    LOG.info("get TaskName for " + activityId);
+    String taskName = activityId;
+    Node taskNode = getNodeFromBpmn(xml, activityId);
+    if (taskNode != null) {
+      taskName = taskNode.getAttributes().getNamedItem("name").getNodeValue();
+    }
+    return taskName;
+  }
+
+  public static String getFormSchemaFromBpmn(String xml, String formId) {
+    LOG.info("get TaskName for " + formId);
+    String schema = null;
+    Node formNode = getNodeFromBpmn(xml, formId);
+    if (formNode != null) {
+      schema = formNode.getFirstChild().getNodeValue();
+    }
+    return schema;
+  }
+
+  public static String getProcessNameFromFile(String bpmnFileName, String bpmnProcessId) {
+    String processName = bpmnProcessId;
+    Node processNode = getNodeFromBpmnFile(bpmnFileName, bpmnProcessId);
+    if (processNode != null) {
+      processName = processNode.getAttributes().getNamedItem("name").getNodeValue();
+    }
+    return processName;
+  }
+
+  public static String getTaskNameFromFile(String bpmnFileName, String activityId) {
+    String taskName = activityId;
+    Node taskNode = getNodeFromBpmnFile(bpmnFileName, activityId);
+    if (taskNode != null) {
+      taskName = taskNode.getAttributes().getNamedItem("name").getNodeValue();
+    }
+    return taskName;
+  }
+
+  public static String getFormSchemaFromFile(String bpmnFileName, String formId) {
+    String schema = null;
+    Node formNode = getNodeFromBpmnFile(bpmnFileName, formId);
+    if (formNode != null) {
+      schema = formNode.getFirstChild().getNodeValue();
+    }
+    return schema;
+  }
+}
